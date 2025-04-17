6611da371fa8454e4fb51e990c22996626261494
Binary Digitz <borisndoh@gmail.com>
2025-04-17 22:50:26 +0100

    Add task lists

--- client/src/context/AppProvider.jsx
+++ client/src/context/AppProvider.jsx
@@ -1,20 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AppContext from './AppContext'
 import { useNavigate } from 'react-router-dom'
+import { todos } from '../assets/assets'
 
 function AppProvider({ children}) {
     const navigate = useNavigate()
     const baseUrl = ''
     const [ user, setUser ] = useState(null)
     const [ token, setToken ] = useState(null)
+    const [ tasks, setTasks ] = useState(null)
 
     const values ={
         baseUrl,
         navigate,
         user, setUser,
-        token, setToken
+        token, setToken,
+        tasks, setTasks
 
     }
+    useEffect(() =>{
+      setTasks(todos)
+    },[])
   return (
     <AppContext.Provider value={values}>
       { children}
--- client/src/pages/MyTasks.jsx
+++ client/src/pages/MyTasks.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useContext } from "react";
+import AppContext from "../context/AppContext";
 
 function MyTasks() {
+  const { tasks } = useContext(AppContext);
+  console.log(tasks);
+
   return (
-    <div>
-      my task
+    <div className="mt-5">
+      <div className="text-center ">
+        <h3 className="heading3">Your To-Do List</h3>
+        <p className="paragraph1 text-neutral-800">
+          Here’s everything you’ve got planned
+        </p>
+        <p className="paragraph1 text-neutral-800">
+          Stay focused, stay productive — you’ve got this!
+        </p>
+
+        <table className=" bg-white h-62 w-sm md:w-md lg:w-xl xl:w-full mx-auto mt-10 ">
+          <thead>
+            <tr className="p-5">
+              <td className="p-4 ">SN</td>
+              <td>TITLE</td>
+              <td>START TIME</td>
+              <td>STATUS</td>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks &&
+              tasks.map((task, index) => (
+                <tr key={task.id} className=" text-gray-700 text-sm hover:bg-green-50">
+                  <td>{index + 1 }</td>
+                  <td>{task.title}</td>
+                  <td>{task.title}</td>
+                  <td>{task.title}</td>
+                  <td className="text-green-500 cursor-pointer hover:bg-green-100">View </td>
+                </tr>
+              ))}
+          </tbody>
+
+          <tbody></tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MyTasks
+export default MyTasks;
